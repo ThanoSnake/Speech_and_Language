8beff9a6714617b11abe8f51f18531dfb3645efd
Emmanouil Chatzigeorgiou <58857221+emhadzi@users.noreply.github.com>
2026-03-20 16:58:04 +0200

    Update and rename 3_4_2.py to 3_4_2.sh

--- Lab1/scripts/3_4_2.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# 3.4.2 - Δημιουργία φακέλων data/train, data/dev, data/test
-# Εκτέλεση από: ~/kaldi/egs/usc/
-
-import os
-
-for split in ['train', 'dev', 'test']:
-    os.makedirs(os.path.join('data', split), exist_ok=True)
-    print(f"Created data/{split}/")
---
+++ Lab1/scripts/3_4_2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir -p usc
+cd usc || exit
+mkdir -p data/train data/dev data/test
