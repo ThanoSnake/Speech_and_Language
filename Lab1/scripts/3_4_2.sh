bd16bcc9aa16e87b4956a9b5afe89f129828a686
Emmanouil Chatzigeorgiou <58857221+emhadzi@users.noreply.github.com>
2026-03-20 20:50:49 +0200

    Delete Lab1/scripts directory

--- Lab1/scripts/3_4_2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mkdir -p usc
-cd usc || exit
-mkdir -p data/train data/dev data/test
--- Lab1/scripts/4_1_5.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# 4.1.5 - Δημιουργία φακέλων για γλωσσικό μοντέλο και γλώσσα
-# Εκτέλεση από: ~/kaldi/egs/usc/
-
-mkdir -p data/lang data/local/dict data/local/lm_tmp data/local/nist_lm
-
-echo "Created: data/lang, data/local/dict, data/local/lm_tmp, data/local/nist_lm"
